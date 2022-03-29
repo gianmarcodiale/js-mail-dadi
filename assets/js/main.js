5495cdba79e0e5eb37d02c750d8d24ea3e1a32ea
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-03-29 18:09:12 +0200

    corrections

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,47 +4,23 @@ ESERCIZIO 1: MAIL
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 */
 
-const userEmailInput = document.getElementById("email");
-
-const userEmail = userEmailInput.value;
-
-const button = document.getElementById("submit-btn");
-
-const emailList = ["tanjirokamado@gmail.com", "uzumaki_naruto@gmail.com", "monkeydluffy@gmail.com", "erenjager@gmail.com"];
+let userEmailInput = document.getElementById("email");
+let userEmail = userEmailInput.value;
+let button = document.getElementById("submit-btn");
+let emailList = ["tanjirokamado@gmail.com", "uzumaki_naruto@gmail.com", "monkeydluffy@gmail.com", "erenjager@gmail.com"];
 
 button.addEventListener("click", 
-
     function() {
-
         for (let index = 0; index < emailList.length; index++) {
-
-            if (userEmail === emailList[index]) {
-
-                console.log("Hai effettuato l'accesso");
-
+            let emailListIndex = emailList[index];
+            if (emailListIndex !== userEmail) {
+                console.log("non registrato");
             } else {
-
-                console.log("Mi dispiace ma la tua email non è stata riconosciuta");
-
+                console.log("registrato");
             }
-        
         }
-
     }
-
 )
-
-
-
-
-
-
-
-
-
-
-
-
 
 /* 
 ESERCIZIO 2: GIOCO DEI DADI
@@ -52,18 +28,14 @@ ESERCIZIO 2: GIOCO DEI DADI
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
-let user_play_input = document.getElementById("dice-input");
 let user_play = Math.floor(Math.random() * 6 + 1);
 let computer_play = Math.floor(Math.random() * 6 + 1);
 let play_btn = document.getElementById("play");
 
 play_btn.addEventListener("click", 
-
     function() {
         if (user_play > computer_play) {
-
             alert(`Hai lanciato il numero ${user_play}. Hai vinto! Il computer ha giocato il numero ${computer_play}.`)
-
         } else {
             alert(`Mi dispiace hai perso! Il computer ha giocato il numero ${computer_play} ed è più alto. La tua giocata è ${user_play}.`)
         }
