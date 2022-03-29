2bdd6b4d6d619476b9a9f64f146547f0e1e67b59
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-03-29 17:34:08 +0200

    complete dice game

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,35 @@ ESERCIZIO 1: MAIL
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 */
 
+const userEmailInput = document.getElementById("email");
 
+const userEmail = userEmailInput.value;
 
+const button = document.getElementById("submit-btn");
 
+const emailList = ["tanjirokamado@gmail.com", "uzumaki_naruto@gmail.com", "monkeydluffy@gmail.com", "erenjager@gmail.com"];
+
+button.addEventListener("click", 
+
+    function() {
+
+        for (let index = 0; index < emailList.length; index++) {
+
+            if (userEmail === emailList[index]) {
+
+                console.log("Hai effettuato l'accesso");
+
+            } else {
+
+                console.log("Mi dispiace ma la tua email non è stata riconosciuta");
+
+            }
+        
+        }
+
+    }
+
+)
 
 
 
@@ -24,5 +50,23 @@ Chiedi all’utente la sua email, controlla che sia nella lista di chi può acce
 ESERCIZIO 2: GIOCO DEI DADI
 
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
-Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
 */
+
+let user_play_input = document.getElementById("dice-input");
+let user_play = Math.floor(Math.random() * 6 + 1);
+let computer_play = Math.floor(Math.random() * 6 + 1);
+let play_btn = document.getElementById("play");
+
+play_btn.addEventListener("click", 
+
+    function() {
+        if (user_play > computer_play) {
+
+            alert(`Hai lanciato il numero ${user_play}. Hai vinto! Il computer ha giocato il numero ${computer_play}.`)
+
+        } else {
+            alert(`Mi dispiace hai perso! Il computer ha giocato il numero ${computer_play} ed è più alto. La tua giocata è ${user_play}.`)
+        }
+    }
+
+)
