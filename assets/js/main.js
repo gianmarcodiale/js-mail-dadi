8d0837fe1a06bb66655a09ce317e0afbda7e31be
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-03-29 16:25:57 +0200

    INITIAL COMMIT

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+/* 
+ESERCIZIO 1: MAIL
+
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+ESERCIZIO 2: GIOCO DEI DADI
+
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+*/
